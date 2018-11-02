b0099b77486e82ae04e49e2a813dfe4874839dec
knaut <info@knaut.net>
2018-11-02 12:29:51 -0700

    using an express router with nested handlers

--- server/index.js
+++ server/index.js
@@ -7,10 +7,34 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-// ROUTES
-app.get('/', function(req, res) {
+// ROUTER
+const router = express.Router();
+
+const handlers = [
+  function(req, res, next) {
+    console.log('this is another message of some kind');
+
+    next();
+  },
+  function(req, res, next) {
+    console.log('this is a route with multiple callbacks');
+
+    next();
+  },
+];
+
+router.use(handlers);
+
+router.get('/', function(req, res) {
   res.send('hello!!');
-}); 
+});
+
+router.get('/test', function(req, res) {
+  res.send('random test');
+});
+
+// ROUTES
+app.use('/home', router);
 
 app.listen(port, function() {
   console.log(`sqlBust listening on ${port}`);
