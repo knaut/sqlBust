98d74b28e5f7fddfe0ed38f5b5f8a71d04ae18e7
knaut <info@knaut.net>
2018-11-02 12:17:55 -0700

    simple express routing

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// IMPORTS
+import express from 'express';
+
+// SETUP
+const app = express();
+const port = 3000;
+
+// ROUTES
+app.get('/', function(req, res) {
+  res.send('hello!!');
+}); 
+
+app.listen(port, function() {
+  console.log(`sqlBust listening on ${port}`);
+});
