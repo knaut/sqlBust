835e3aa4e79e8591ec535d025854bcdaeae10c97
knaut <info@knaut.net>
2018-11-02 16:48:01 -0700

    api call made promise style

--- server/index.js
+++ server/index.js
@@ -3,13 +3,11 @@
 // IMPORTS
 import express from 'express';
 import chalk from 'chalk';
+import mysql from 'mysql';
 
 // SETUP
 const app = express();
 const port = 3000;
-
-// ROUTER
-const router = express.Router();
 
 const loggers = [
   function(req, res, next) {
@@ -17,7 +15,8 @@ const loggers = [
       chalk.green(req.method),
       chalk.gray(req.headers.host),
       chalk.green(req.url),
-      chalk.blue('Body:', req.body)
+      chalk.blue('Body:'),
+      res.body
     );
     next();
   }
@@ -25,9 +24,38 @@ const loggers = [
 
 app.use(loggers);
 
+const connection = mysql.createConnection({
+  host: '0.0.0.0',
+  user: 'root',
+  password: '',
+  database: 'magnitude'
+});
+
+function getLessons(connection) {
+  return new Promise( function( resolve, reject ) {
+    var query = `select * from lessons`;
+    connection.connect();
+    connection.query(query, function(error, results, fields) {
+      if (error) {
+        console.log(error);
+        return reject(error);
+      }
+      return resolve( results );
+    });
+  });
+}
+
 // ROUTES
-app.use('/', function(req, res) {
-  res.send('hello!');
+app.get('/', function(req, res) {
+
+  getLessons( connection ).then(results => {
+    res.send(results)
+  });
+
+});
+
+app.post('/', function(req, res) {
+  res.send('posted!');
 });
 
 app.listen(port, function() {
