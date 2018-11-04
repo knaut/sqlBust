3705e7b0eb39cc66993913b9029fa5ab86984d51
_knaut <info@knaut.net>
2018-11-04 14:54:28 -0800

    added grommet v2 to client

--- client/index.jsx
+++ client/index.jsx
@@ -2,10 +2,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// COMPONENTS
+import { Grommet, Box, Button, Grid, Text } from "grommet";
+import { grommet } from "grommet/themes";
+
+const NColumnGrid = () => (
+  <Grommet theme={grommet} full>
+    <Grid
+      columns={{
+        count: 6,
+        size: "auto"
+      }}
+      gap="small"
+    >
+      <Box background="brand">Item 1</Box>
+      <Box background="brand">Item 2</Box>
+      <Box background="brand">Item 3</Box>
+      <Box background="brand">Item 4</Box>
+      <Box background="brand">Item 5</Box>
+      <Box background="brand">Item 6</Box>
+    </Grid>
+  </Grommet>
+);
+
 // RENDER CLIENT
 ReactDOM.render(
 	<div id="global-wrapper">
 		<p>This is the app</p>
+    <NColumnGrid />
 	</div>,
 	document.getElementById('root')
 );
+
+console.log(Grommet);
+
+
