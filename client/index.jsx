a71214d871cdd667ad86e461ac80a679375a5960
_knaut <info@knaut.net>
2018-11-04 16:15:34 -0800

    grommet v2 basic layout

--- client/index.jsx
+++ client/index.jsx
@@ -1,39 +1,75 @@
 // IMPORTS
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 // COMPONENTS
 import { Grommet, Box, Button, Grid, Text } from "grommet";
 import { grommet } from "grommet/themes";
 
-const NColumnGrid = () => (
-  <Grommet theme={grommet} full>
-    <Grid
-      columns={{
-        count: 6,
-        size: "auto"
-      }}
-      gap="small"
-    >
-      <Box background="brand">Item 1</Box>
-      <Box background="brand">Item 2</Box>
-      <Box background="brand">Item 3</Box>
-      <Box background="brand">Item 4</Box>
-      <Box background="brand">Item 5</Box>
-      <Box background="brand">Item 6</Box>
-    </Grid>
-  </Grommet>
-);
+class AppGrid extends Component {
+  state = { sidebar: true };
+
+  render() {
+    const { sidebar } = this.state;
+    return (
+      <Grommet theme={grommet}>
+        <Grid
+          rows={["auto", "medium"]}
+          columns={["auto", "flex"]}
+          areas={[
+            { name: "header", start: [0, 0], end: [1, 0] },
+            { name: "sidebar", start: [0, 1], end: [0, 1] },
+            { name: "main", start: [1, 1], end: [1, 1] }
+          ]}
+        >
+          <Box
+            gridArea="header"
+            direction="row"
+            align="center"
+            justify="between"
+            pad={{ horizontal: "medium", vertical: "small" }}
+            background="dark-2"
+          >
+            <Button onClick={() => this.setState({ sidebar: !sidebar })}>
+              <Text size="large">Title</Text>
+            </Button>
+            <Text>my@email</Text>
+          </Box>
+          {sidebar && (
+            <Box
+              gridArea="sidebar"
+              background="dark-5"
+              width="small"
+              animation={[
+                { type: "fadeIn", duration: 300 },
+                { type: "slideRight", size: "xlarge", duration: 150 }
+              ]}
+            >
+              {["First", "Second", "Third"].map(name => (
+                <Button key={name} href="#" hoverIndicator>
+                  <Box pad={{ horizontal: "medium", vertical: "small" }}>
+                    <Text>{name}</Text>
+                  </Box>
+                </Button>
+              ))}
+            </Box>
+          )}
+          <Box gridArea="main" justify="center" align="center">
+            <Text>main</Text>
+          </Box>
+        </Grid>
+      </Grommet>
+    );
+  }
+}
 
 // RENDER CLIENT
 ReactDOM.render(
 	<div id="global-wrapper">
-		<p>This is the app</p>
-    <NColumnGrid />
+		<AppGrid />
 	</div>,
 	document.getElementById('root')
 );
 
-console.log(Grommet);
 
 
